99a0b352de8c760928bd2e0beb3dbac4221f9e58
Elif Balci <mrselifbalci@gmail.com>
2021-05-20 23:50:50 +0100

    sign in routes updated

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -4,8 +4,8 @@ const userControllers = require("../controllers/users.controllers")
 const isAdmin = require("../auth/isAdmin")
 
 router.get("/users",userControllers.getAllUsers)
-router.post("/users",userControllers.createUser)
-router.post("/users/login", userControllers.login)
+router.post("/users/signup",userControllers.createUser)
+router.post("/users/signin", userControllers.login)
 router.put("/users/:id",isAdmin, userControllers.updateUser)
 router.delete("/users/:id",isAdmin, userControllers.deleteUser)
 
