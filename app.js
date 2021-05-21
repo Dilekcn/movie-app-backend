121037ed2dd8536b39a4554368a27646d23da7ff
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-05-21 10:19:15 +0100

    added faq router in app.js

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users.routes');
-const postsRouter = require("./routes/posts.routes")
+const postsRouter = require('./routes/posts.routes');
+const faqsRouter = require('./routes/faqs.routes');
 
 //middlewares
-const verifyToken = require("./auth/verifyToken")
-
+const verifyToken = require('./auth/verifyToken');
 
 var app = express();
 
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 //DB connection
-require("./config/db.config")()
+require('./config/db.config')();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -29,22 +29,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/', usersRouter);
-app.use("/",verifyToken, postsRouter)
+app.use('/', postsRouter);
+app.use('/', faqsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
--- routes/faqs.routes.js
+++ routes/faqs.routes.js
@@ -4,3 +4,5 @@ const router = express.Router();
 const faqControllers = require('../controllers/faqs.controllers');
 
 router.get('/faqs', faqControllers.getAllFaqs);
+
+module.exports = router;
