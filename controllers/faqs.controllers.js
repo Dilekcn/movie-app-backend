a16a87740c4117cdf192affd360cb8c22ec6a79a
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-05-21 10:25:06 +0100

    added createFaq function

--- controllers/faqs.controllers.js
+++ controllers/faqs.controllers.js
@@ -10,3 +10,23 @@ exports.getAllFaqs = async (req, res) => {
 		res.status(500).json(error);
 	}
 };
+
+exports.createFaq = async (req, res) => {
+	const newFaq = await new FaqModel({
+		question: req.body.question,
+		answer: req.body.answer,
+		isActive: req.body.isActive,
+		isDeleted: req.body.isDeleted,
+	});
+
+	newFaq
+		.save()
+		.then((data) =>
+			res.json({
+				status: true,
+				message: 'Added new faq successfully.',
+				data,
+			}),
+		)
+		.catch((err) => res.json({ status: false, message: err }));
+};
