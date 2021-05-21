8f735faa6fdb76df899441bdf16437a4cdf7e05b
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-05-21 10:15:22 +0100

    added faqs route in routes

---
+++ routes/faqs.routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+const faqControllers = require('../controllers/faqs.controllers');
+
+router.get('/faqs', faqControllers.getAllFaqs);
