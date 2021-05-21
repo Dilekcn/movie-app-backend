b8fac621fc03cee50b74bdbdc3fc3bdeb29ad784
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-05-21 19:11:03 +0100

    added contactinfo controller and route

--- model/ContactInfo.model.js
+++ model/ContactInfo.model.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ContactInfoSchema = new Schema({
-	address: { type: String, required: true },
-	phone: { type: String, required: true },
-	email: { type: String, required: true },
-	socialmedia: { type: Array, required: true },
-});
+const ContactInfoSchema = new Schema(
+	{
+		address: { type: String, required: true },
+		phone: { type: String, required: true },
+		email: { type: String, required: true },
+		socialMedia: { type: Array, required: true },
+		isActive: { type: Boolean, default: true },
+		isDeleted: { type: Boolean, default: false },
+		userId: { type: mongoose.Types.ObjectId },
+	},
+	{ timestamps: true },
+);
 
 module.exports = mongoose.model('contactinfo', ContactInfoSchema);
