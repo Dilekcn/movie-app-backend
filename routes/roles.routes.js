ac0d549d67f99fa7ea8d0282874801e9cd1b6b2b
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-05-25 06:28:27 +0100

    added create role router

--- routes/roles.routes.js
+++ routes/roles.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const rolesControllers = require('../controllers/roles.controllers');
 
 router.get('/roles', rolesControllers.getAllRoles);
+router.post('/roles', rolesControllers.createRole);
 
 module.exports = router;
