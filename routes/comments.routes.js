f7e972c32bf0e7f76069dd806c1162b554a9e425
Elif Balci <mrselifbalci@gmail.com>
2021-06-12 16:22:49 +0100

    findOne switched with find for commentsByUserId

--- routes/comments.routes.js
+++ routes/comments.routes.js
@@ -5,8 +5,8 @@ const commentsControllers = require('../controllers/comments.controllers');
  
 router.get("/comments",commentsControllers.getAll)
 router.get("/comments/:id",commentsControllers.getSingleComment)
-router.get("/comments/userid/:userId",commentsControllers.getSingleCommentByUserId)
-router.get("/comments/videoid/:videoId",commentsControllers.getSingleCommentByVideoId)
+router.get("/comments/userid/:userId",commentsControllers.getCommentsByUserId)
+router.get("/comments/videoid/:videoId",commentsControllers.getCommentsByVideoId)
 router.post("/comments",commentsControllers.create)
 router.put("/comments/:id", commentsControllers.updateComment)
 router.delete("/comments/:id", commentsControllers.removeSingleComment)
