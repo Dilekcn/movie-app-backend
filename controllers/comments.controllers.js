a4d2c26c03eecad2a530971a33d6ad052956ba24
Elif Balci <mrselifbalci@gmail.com>
2021-06-19 20:13:58 +0100

    populate updated for comments

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -4,9 +4,7 @@ const CommentsModel = require("../model/Comment.model")
 exports.getAll = async (req, res) => {
   try {
     const response = await CommentsModel.find()
-    .populate({ path: 'userId', select: 'firstname lastname' })
-    // .populate('userId','firstname')
-    
+    .populate('userId','firstname lastname')
     res.json(response)
   } catch (error) { 
     res.status(500).json(error)
