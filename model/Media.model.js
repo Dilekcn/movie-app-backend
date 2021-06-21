6d36ac34a1d3eae16055d79b2913cb98162637c0
Elif Balci <mrselifbalci@gmail.com>
2021-06-21 14:13:25 +0100

    media model required removed

--- model/Media.model.js
+++ model/Media.model.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const MediaSchema = new Schema({ 
-    url: {type:String, required: true},
-    title:{type:String, required: true},
-    description:{type:String, required: true},
+    url: {type:String},
+    title:{type:String},
+    description:{type:String},
     isActive:{type:Boolean, default:true},
     isDeleted:{type:Boolean, default:false},
 }, {timestamps: true})
-
+ 
 
 module.exports = mongoose.model('media', MediaSchema)
