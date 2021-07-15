321870fa556f6310d66c38213df1f960850e195f
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-07-15 23:56:55 +0100

    updated Movies model

--- model/Movies.js
+++ model/Movies.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const MoviesSchema = new Schema({});
+const MoviesSchema = new Schema({
+	title: { type: String },
+});
 
 module.exports = mongoose.model('movies', MoviesSchema);
