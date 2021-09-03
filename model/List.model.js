7543fd3917aa12bbed7196a2eeba8fe1ed496bc3
Elif Balci <mrselifbalci@gmail.com>
2021-09-03 18:16:37 +0100

    .

--- model/List.model.js
+++ model/List.model.js
@@ -5,12 +5,10 @@ const ListsSchema = new Schema(
 	{
 		userId: { type: mongoose.Types.ObjectId, ref: 'user' },
 		likes: [{ type: mongoose.Types.ObjectId, ref: 'user' }],
-		// commentIds: [{ type: mongoose.Types.ObjectId, ref: 'comment' }],
 		name: { type: String, required: true },
 		description: { type: String, required: true },
 		rating:{ type: Number, default: 0 }, 
 		tags: { type: Array },
-		// userRatingIds:[{ type: mongoose.Types.ObjectId, ref: 'userrating' }],
 		movieIds:[{ type: mongoose.Types.ObjectId, ref: 'movie' }],
 		isPublic: { type: Boolean, default: true },
 		isActive: { type: Boolean, default: false }, 
@@ -20,4 +18,4 @@ const ListsSchema = new Schema(
 ); 
 
 module.exports = mongoose.model('list', ListsSchema); 
-  
+   
