9b071388c53efcaddadb7058a20bf1c1c00c897e
Elif Balci <mrselifbalci@gmail.com>
2021-09-06 19:52:20 +0100

    .

--- controllers/lists.controllers.js
+++ controllers/lists.controllers.js
@@ -90,6 +90,92 @@ exports.getAll =async (req,res)=>{
 }
 
 
+exports.getPopular =async (req,res)=>{
+
+	const{page=1,limit=10}=req.query
+	const total = await ListsModel.find().countDocuments();
+	await ListsModel.aggregate(
+	[
+		{$sort:{rating: -1}},  
+		{$skip:(page - 1) * limit}, 
+		{$limit:limit*1},
+		{
+            $lookup:{ 
+				from:'movies',
+				let:{"movieIds":"$movieIds"},
+				pipeline:[
+					{$match:{$expr:{$in:["$_id","$$movieIds"]}}},
+					{$project:{type:1,imdb_id:1,imdb_rating:1,original_title:1,image_path:1,backdrop_path:1}},
+				],
+				as:'movieIds' 
+			} 
+		},
+		{
+            $lookup:{
+				from:'users',
+				let:{"userId":"$userId"},
+				pipeline:[
+					{$match:{$expr:{$eq:["$_id","$$userId"]}}},
+					{$project:{firstname:1,lastname:1,mediaId:1}},  
+						{
+						$lookup:{
+							from:'media',
+							let:{"mediaId":"$mediaId"},
+							pipeline:[
+								{$match:{$expr:{$eq:["$_id","$$mediaId"]}}},
+								{$project:{url:1}},
+							],
+							as:'mediaId'   
+						}
+					}
+				],
+				as:'userId'
+			} 
+		},
+		
+		{
+            $lookup:{
+				from:'comments',
+				localField:"_id",
+				foreignField:'listId', 
+				as:'commentIds'
+			}  
+		},
+		{
+            $lookup:{
+				from:'users', 
+				let:{"likes":"$likes"},
+				pipeline:[ 
+					{$match:{$expr:{$in:["$_id","$$likes"]}}},
+					{$project:{firstname:1,lastname:1}}
+				],
+				as:'likes'
+			} 
+		},
+		{
+            $lookup:{
+				from:'userratings',
+				localField:"_id", 
+				foreignField:'listId', 
+				as:'userRatingIds'
+			} 
+		},
+		{
+			$project:{
+				likes:true,rating:true,tags:true,movieIds:true,isPublic:true,isActive:true,isDeleted:true,userId:true,name:true,description:true,'userRatingIds.rating':true,'userRatingIds.userId':true,'commentIds.userId':true,'commentIds.title':true,'commentIds.content':true
+			} 
+		},
+
+		
+	],
+	(err,response)=>{
+	if(err)res.json(err);
+	const pages = limit === undefined ? 1 : Math.ceil(total / limit);
+	res.json({ total,pages, status: 200, response })
+}) 
+}
+
+
  
 
 exports.create = async (req, res) => {
--- routes/lists.routes.js
+++ routes/lists.routes.js
@@ -4,12 +4,13 @@ var router = express.Router();
 const listsControllers = require('../controllers/lists.controllers');
 
 router.get('/lists', listsControllers.getAll);
+router.get('/lists/populars', listsControllers.getPopular);
 router.get('/lists/:id', listsControllers.getSingleList);
 router.get('/lists/userid/:id', listsControllers.getListByUserId);
 router.post('/lists', listsControllers.create);
 router.put('/lists/:id', listsControllers.updateList);
 router.put('/lists/removemovies/:id', listsControllers.removeMovieFromList);
-router.delete('/lists/:id', listsControllers.removeSingleList); 
+router.delete('/lists/:id', listsControllers.removeSingleList);    
 
-module.exports = router;  
+module.exports = router;   
  
