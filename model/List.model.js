bb5d7647276914839a00b639ab7f37a348fc09ac
Elif Balci <mrselifbalci@gmail.com>
2021-09-07 12:33:50 +0100

    .

--- model/List.model.js
+++ model/List.model.js
@@ -9,7 +9,7 @@ const ListsSchema = new Schema(
 		description: { type: String, required: true },
 		rating:{ type: Number, default: 0 },  
 		tags: { type: Array },
-		movieIds:[{ type: mongoose.Types.ObjectId, ref: 'movie' }],
+		movieIds:[{ type: mongoose.Types.ObjectId, ref: 'movie',required:true }],
 		isPublic: { type: Boolean, default: true },
 		isActive: { type: Boolean, default: false }, 
 		isDeleted: { type: Boolean, default: false },  
